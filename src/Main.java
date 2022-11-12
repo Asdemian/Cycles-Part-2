b97c5bc548b034146bb2daefac3e3fe4ecca1ee3
hw commit <demaniks@yahoo.com>
2022-11-12 15:33:25 +0300

    Задание 1-1

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,16 @@
 public class Main {
     public static void main(String[] args) {
-
+        //task1
+        int month = 1;
+        double accumulation = 0;
+        double cashDeposit = 15000;
+        double purpose = 2_459_000;
+        double percent = cashDeposit * 0.12;
+        while (accumulation <= purpose) {
+            accumulation = accumulation + cashDeposit;
+            System.out.println("Месяц " + month + " , сумма накоплений равна " + accumulation + " рублей.");
+            accumulation = accumulation + percent;
+            month++;
+        }
     }
 }
